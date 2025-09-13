2a152be47795246fc518c7bfff7fad6bb9626b4c
Vincent Crooks <crooksv@usf.edu>
2025-09-13 18:25:58 -0400

    dataset

--- cc_3b.js
+++ cc_3b.js
@@ -3,3 +3,10 @@
 // resubmitting inclass assignment from 9/10
 // I had to make a new GitHub Repository to submit correctly
 
+let inventory = [
+    {sku:"SKU-001",name:"TACOS",price:.99,stock:5}
+    {sku:"SKU-002",name:"BURRITO",price:8.00,stock:4}
+    {sku:"SKU-003",name:"PIZZA",price:10.00,stock:3}
+    {sku:"SKU-004",name:"BURGER",price:6.00,stock:8}
+]
+console.log ("test commit")
